07513fba5026411b5e0c8f99497f8f7cf492f436
Asia <nastysha14@yandex.ru>
2022-10-18 14:18:32 +0300

    2

---
+++ seminar1/ex.2/Program.cs
@@ -0,0 +1,23 @@
+﻿int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+int i = 0;
+
+if (a > b)
+{
+   i = a;
+}
+if (a < b)
+{
+   i = b;
+}
+
+if (i > c)
+{
+ Console.WriteLine(i);
+}
+if (i < c)
+{
+   Console.WriteLine(c);
+}
+
