a7d2666623e4259af270f54b2868924c207810a6
Asia <nastysha14@yandex.ru>
2022-10-19 13:27:26 +0300

    Второе задание

---
+++ seminar2/ex.2/Program.cs
@@ -0,0 +1,15 @@
+﻿int number = new Random().Next(1,999);
+
+Console.WriteLine(number);
+int a = (number%10)%10;
+
+
+if (number>99)
+{
+   Console.WriteLine(a);
+}
+else
+{
+   Console.WriteLine("Третьей цифры нет"); 
+}
+
