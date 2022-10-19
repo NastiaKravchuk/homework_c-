03c3afee9d51822f3dd7c504266ecee5e3ea3e10
Asia <nastysha14@yandex.ru>
2022-10-19 13:03:08 +0300

    Первое задание

---
+++ seminar2/ex.1/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+int number = new Random().Next(100,999);
+Console.WriteLine(number);
+int a = (number%100)/10;
+Console.WriteLine(a);
