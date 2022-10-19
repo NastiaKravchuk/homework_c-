81fb6ba9dbf97704fd2aa4b8d0d8dda15905de05
Asia <nastysha14@yandex.ru>
2022-10-19 14:29:08 +0300

    Третье Задание

---
+++ seminar2/ex.3/Program.cs
@@ -0,0 +1,19 @@
+﻿int a = int.Parse(Console.ReadLine());
+
+
+switch(a)
+{
+    case 1: Console.WriteLine("Понедельник"); break;
+    case 2: Console.WriteLine("Вторник"); break;
+    case 3: Console.WriteLine("Среда"); break;
+    case 4: Console.WriteLine("Четверг"); break;
+    case 5: Console.WriteLine("Пятница"); break;
+    case 6: Console.WriteLine("Суббота, Выходной"); break;
+    case 7: Console.WriteLine("Воскресенье, Выходной"); break;
+}
+
+
+
+
+
+
