d3973bed71d2dd27995a3580d9c742502c994952
Asia <nastysha14@yandex.ru>
2022-10-21 16:29:22 +0300

    Домашнее задание к 3-ему семинару

---
+++ seminar3/ex.1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine ("Введите пятизначное число");
+int a = int.Parse(Console.ReadLine());
+string str = a.ToString();
+int [] array = new int [str.Length];
+
+
+for (int i = 0; i < array.Length; i++)
+{ array[i]=int.Parse(str[i].ToString());}
+
+    if ( array[0]==array[4] && array[1]==array[3] )
+    {
+      Console.WriteLine("Tru");  
+    }
+    else
+    {
+        Console.WriteLine("False");
+    }
---
+++ seminar3/ex.2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine ("Ведите координаты первой точки");
+int x1 = int.Parse(Console.ReadLine());
+int y1 = int.Parse(Console.ReadLine());
+int z1 = int.Parse(Console.ReadLine());
+
+int [] array1 = new int [] {x1,y1,z1};
+
+Console.WriteLine ("Ведите координаты второй точки");
+int x2 = int.Parse(Console.ReadLine());
+int y2 = int.Parse(Console.ReadLine());
+int z2 = int.Parse(Console.ReadLine());
+
+int [] array2 = new int [] {x2,y2,z2};
+
+double result = Math.Sqrt(Math.Pow((array1[0]-array2[0]),2) +Math.Pow((array1[1]-array2[1]),2) + Math.Pow((array1[2]-array2[2]),2));
+Console.WriteLine($" Результат {result}");
+
