3761b380f2d69a7840713c33948b14c1a0875bcb
Asia <nastysha14@yandex.ru>
2022-10-26 01:32:29 +0300

    д\з шестой семинар

---
+++ seminar6/ex.1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите числа через пробел ");
+string text = Console.ReadLine();
+int[] array = Array.ConvertAll(text.Split(), int.Parse);
+
+
+int GetCount(int[] array)
+{
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+  if (array[i] > 0)
+ {
+  count++;
+}
+}
+  return count;
+}
+Console.WriteLine($"Количество чисел больше 0 = " + (GetCount(array)));
---
+++ seminar6/ex.2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Ведите значение b1");
+int b1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Ведите значение k1");
+int k1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Ведите значение b2");
+int b2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Ведите значение k2");
+int k2 = int.Parse(Console.ReadLine());
+
+double x= (b1-b2)/(k2-k1);
+double y =(k1*x+b1);
+
+
+Console.WriteLine();
+Console.WriteLine(x);
+Console.WriteLine(y);
